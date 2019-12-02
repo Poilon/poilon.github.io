7c0df1a4ea8a3724794e873febcf154c25092721
Poilon <poilon@gmail.com>
2019-12-02 14:30:21 +0100

    test

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $subject 	= $_POST['subject'];
 $msg 		= $_POST['msg'];
 $output 	= "Name: ".$fname."\n\nSubject: ".$subject."\n\nMessage: ".$msg;
 
-$to 		= 'qualzz20@gmail.com';
+$to 		= 'poilonsoftware@gmail.com';
 $headers	= 'FROM: "'.$email.'"';
 
 $send = mail($to, $fname, $output."\n\n***This message has been sent from Martxa", $headers);
