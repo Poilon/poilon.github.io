51c44308b75ff104e11e571d803cad7a1191466d
mathilde <mathilde.charpiot@epitech.eu>
2019-12-03 14:35:21 +0100

    v2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -411,4 +411,13 @@
 			return false;
 	});
 
+	$('ul a').click(function(event) {
+		var id = $(this).attr("href");
+		var offset = 70;
+		var target = $(id).offset().top - offset;
+		$('html, body').animate({
+			scrollTop: target
+		}, 500);
+	});
+
 })(jQuery);
