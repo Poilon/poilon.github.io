1b8146e5edcc8d9062fda15fd1fce68b941b8d67
Poilon <poilon@gmail.com>
2025-11-08 15:09:23 +0100

    remove email php

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -363,53 +363,6 @@
 
 	$('.ui-slider-handle:eq(1)').append('<span class="price-range-max value">$' + $('#slider').slider('values', 1 ) + '</span>');
 
-
-	/*---------------------
-	// Ajax Contact Form
-	--------------------- */
-
-	$('.cf-msg').hide();
-		$('form#cf button#submit').on('click', function() {
-			var fname = $('#fname').val();
-			var subject = $('#subject').val();
-			var email = $('#email').val();
-			var msg = $('#msg').val();
-			var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-
-			if (!regex.test(email)) {
-				alert('Please enter valid email');
-				return false;
-			}
-
-			fname = $.trim(fname);
-			subject = $.trim(subject);
-			email = $.trim(email);
-			msg = $.trim(msg);
-
-			if (fname != '' && email != '' && msg != '') {
-				var values = "fname=" + fname + "&subject=" + subject + "&email=" + email + " &msg=" + msg;
-				$.ajax({
-					type: "POST",
-					url: "mail.php",
-					data: values,
-					success: function() {
-						$('#fname').val('');
-						$('#subject').val('');
-						$('#email').val('');
-						$('#msg').val('');
-
-						$('.cf-msg').fadeIn().html('<div class="alert alert-success"><strong>Success!</strong> Email has been sent successfully.</div>');
-						setTimeout(function() {
-							$('.cf-msg').fadeOut('slow');
-						}, 4000);
-					}
-				});
-			} else {
-				$('.cf-msg').fadeIn().html('<div class="alert alert-danger"><strong>Warning!</strong> Please fillup the informations correctly.</div>')
-			}
-			return false;
-	});
-
 	$('ul a').click(function(event) {
 		var id = $(this).attr("href");
 		var offset = 70;
